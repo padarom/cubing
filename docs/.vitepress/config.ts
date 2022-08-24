5860abd9fa93135b64efa640f1dfba164ed403ea
Christopher Mühl <christopher@muehl.dev>
2022-08-24 14:29:20 +0200

    Configure PWA requirements

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -15,7 +15,12 @@ export default defineConfig({
   buildEnd: buildPwa,
 
   head: [
-  //  ['script', { src: '/registerSW.js' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
+    ['link', { rel: 'mask-icon', href: '/mask-icon.svg', color: '#FFFFFF' }],
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+    ['script', { src: '/registerSW.js' }],
   ],
 
   themeConfig: {
--- docs/.vitepress/pwa.ts
+++ docs/.vitepress/pwa.ts
@@ -3,12 +3,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 import type { VitePluginPWAAPI } from 'vite-plugin-pwa'
 
 export const PwaPlugin = VitePWA({
-  outDir: '.vitepress/dist',
+  outDir: 'docs/.vitepress/dist',
   registerType: 'autoUpdate',
-  includeManifestIcons: false,
-  devOptions: {
-    enabled: true,
-  },
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
   manifest: {
     id: '/',
     name: 'Padarom\'s Cubing Library',
@@ -33,63 +30,14 @@ export const PwaPlugin = VitePWA({
       },
     ],
   },
-  workbox: {
-    globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
-    runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'gstatic-fonts-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-      {
-        urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'jsdelivr-images-cache',
-          expiration: {
-            maxEntries: 10,
-            maxAgeSeconds: 60 * 60 * 24 * 7, // <== 7 days
-          },
-          cacheableResponse: {
-            statuses: [0, 200],
-          },
-        },
-      },
-    ],
-  },
 })
 
 export async function buildPwa() {
-  const config = await resolveConfig({}, 'build', 'production')
+  const config = await resolveConfig({ plugins: [PwaPlugin] }, 'build', 'production')
   
   // when `vite-plugin-pwa` is presented, use it to regenerate SW after rendering
   const pwaPlugin: VitePluginPWAAPI = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
   if (pwaPlugin && pwaPlugin.generateSW && !pwaPlugin.disabled) {
     await pwaPlugin.generateSW()
   }
-
-  console.log('Generated Service WOrker')
 }
